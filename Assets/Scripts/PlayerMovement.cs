8b85638935905fae2f0f2ef19df00ed82dc9e0da
Xpyto <69775025+Xpyto@users.noreply.github.com>
2021-04-20 14:23:37 +1000

    WorkInProgress

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,12 +8,14 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;    //Movespeed of the player
     public Transform movePoint;     //The point on the grid the player is moving towards
     public LayerMask Boundary;      //check if there is a boundary
-
+    public int checkGround;
 
     // Start is called before the first frame update
     void Start()
     {
         movePoint.parent = null;
+        checkGround = 0;
+        
     }
 
     // Update is called once per frame
@@ -22,9 +24,12 @@ public class PlayerMovement : MonoBehaviour
 
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
 
-        if(Vector3.Distance(transform.position, movePoint.position) <= 0.02f){
+        if(Vector3.Distance(transform.position, movePoint.position) <= 0.01f){
 
-            if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f){
+            if(checkGround == 2){
+                movePoint.position -= new Vector3(0f,1f,0f);
+            }
+             else if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f){
                 if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), 0.2f, Boundary)){
                 movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
                 }
@@ -34,5 +39,12 @@ public class PlayerMovement : MonoBehaviour
                 }
             }
         }
+
     }
+
+    void OnCollisionEnter2D(Collision2D collision){
+        Debug.Log(collision.collider.name);
+    }
+
+    
 }
